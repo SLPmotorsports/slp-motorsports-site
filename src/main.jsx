09937cbf6d9c005a7f4398687dc471a41ce33694
SLPmotorsports <kenny.zughbi@gmail.com>
2025-06-16 19:34:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";          // optional – create if you want global CSS
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
